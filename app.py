694f39d8f5ffde694f6f20473a206046dd4acad2
Rudra Tiwari <rudrat1841@gmail.com>
2025-10-27 22:28:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import sentry_sdk
+sentry_sdk.init(
+    dsn="https://59779dcff8aafa598ede055ada86bcb1@o4510262460481536.ingest.us.sentry.io/4510262500589568",
+    traces_sample_rate=1.0
+)
+
+
 import streamlit as st
 import tensorflow as tf
 import numpy as np
@@ -38,4 +45,5 @@ if submitted:
     prob = float(model.predict(input_scaled)[0][0])
     pred = int(prob > 0.5)
     st.success(f"Churn prediction: **{'YES' if pred else 'NO'}** (Probability: {prob:.2f})")
+
 
