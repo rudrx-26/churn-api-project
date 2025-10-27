4936f6442d1a8603b46644bc2540a56ef1df224d
Rudra Tiwari <rudrat1841@gmail.com>
2025-10-27 22:30:53 +0530

    Refactor app.py for customer churn prediction

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ sentry_sdk.init(
     dsn="https://59779dcff8aafa598ede055ada86bcb1@o4510262460481536.ingest.us.sentry.io/4510262500589568",
     traces_sample_rate=1.0
 )
+1/0
 
 
 import streamlit as st
@@ -45,5 +46,6 @@ if submitted:
     prob = float(model.predict(input_scaled)[0][0])
     pred = int(prob > 0.5)
     st.success(f"Churn prediction: **{'YES' if pred else 'NO'}** (Probability: {prob:.2f})")
+
 
 
